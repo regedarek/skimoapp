64c0a2ab350fbfdd532632e42f95b7c1deaeadf4
Darek Finster <dariusz.finster@gmail.com>
2020-06-20 12:39:44 +0200

    init site

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,8 @@ require("bootstrap");
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'css/event'
+import 'css/application'
+import 'css/pages'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
