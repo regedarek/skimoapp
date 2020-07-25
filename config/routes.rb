1b788a6a616d11ead9f780d0c834ef2df5d85c23
Darek Finster <dariusz.finster@gmail.com>
2020-07-26 01:54:19 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get 'zaloguj', to: 'devise/sessions#new'
+  end
+
+  authenticated :user do
+    root :to => "high_voltage/pages#show", id: 'tablica'
   end
 end
