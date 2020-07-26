36201f67d28d1a587d38424f560b0db3069886df
Darek Finster <dariusz.finster@gmail.com>
2020-07-26 02:03:13 +0200

    k

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,4 @@ Rails.application.routes.draw do
     get 'zaloguj', to: 'devise/sessions#new'
   end
 
-  authenticated :user do
-    root :to => "high_voltage/pages#show", id: 'tablica'
-  end
 end
