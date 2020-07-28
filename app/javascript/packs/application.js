bbffbba2e01e5e8fe42fbb1f54dc8eb435af2c6b
Darek Finster <dariusz.finster@gmail.com>
2020-07-28 09:57:59 +0200

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'css/application'
 import 'css/pages'
 import 'css/footer'
+import 'slick-carousel'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,6 +25,26 @@ import 'css/footer'
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//
 
-
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
